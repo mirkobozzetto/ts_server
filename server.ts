86425fc50669fcca8bbdc8455b28f46dad8a008a
mirkobozzetto <mirkouch@gmail.com>
2024-08-10 00:33:37 +0200

    Add URL parsing and define Handlers interface in server.ts for improved request handling

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import http from "http";
+import url from "url";
 
 const port = 3000;
 
@@ -14,3 +15,20 @@ type Handler = (
 	data: RequestData,
 	callback: (statusCode: number, payload: object) => void
 ) => void;
+
+interface Handlers {
+	[key: string]: Handler | { [subKey: string]: Handler };
+	notFound: Handler;
+	home: Handler;
+	data: Handler;
+	_data: {
+		get: Handler;
+		post: Handler;
+	};
+}
+
+const server: http.Server = http.createServer(
+	(req: http.IncomingMessage, res: http.ServerResponse) => {
+		const parsedUrl: url.UrlWithParsedQuery = url.parse(req.url || "", true);
+	}
+);
